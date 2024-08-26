8a0b4bb8098ce93aac53f206ecad7e70e28060f1
jincy-dev <jincydev3007@gmail.com>
2024-08-26 20:56:17 +0530

    Added form

---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import noProjectSelectedImage from "../assets/no-projects.png";
+import Button from "./Button";
+function NoProjectSelected({ onStartAddProject }) {
+  return (
+    <div className=" mt-24 text-center w-2/3">
+      <img
+        src={noProjectSelectedImage}
+        alt="emphy task list"
+        className="w-16 h-16 object-containe mx-auto"
+      />
+      <h2 className="text-xl font-cold text-stone-500 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 mb-4">
+        Select a project to get started with new one
+      </p>
+      <p className="mt-8">
+        <Button onClick={onStartAddProject}>Create new project</Button>
+      </p>
+    </div>
+  );
+}
+
+export default NoProjectSelected;
